f35937cf1f0dc637b5838de654591af82d94316e
Volt529 <sylvainjeannincornette@gmail.com>
2026-02-04 09:39:36 +0100

    Add files via upload

---
+++ oc_lettings_site/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('lettings/', include('lettings.urls')),
+    path('profiles/', include('profiles.urls')),
+    path('admin/', admin.site.urls),
+    path('sentry-debug/', views.trigger_error),  # Route de test Sentry
+]
+
+# Handlers pour les erreurs
+handler404 = 'oc_lettings_site.views.custom_404'
+handler500 = 'oc_lettings_site.views.custom_500'
